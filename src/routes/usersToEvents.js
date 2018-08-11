95410141f4dcf89884fa739cc1ee297086a046ff
Callum Marriage <callum.marriage@capgemini.com>
2018-08-11 23:24:38 +0100

    added filters and functions

--- src/routes/usersToEvents.js
+++ src/routes/usersToEvents.js
@@ -23,8 +23,7 @@ export function getEventsBySubscriberRoute(req, res){
                             getEventsById(req.id, events.rows[i].event_id, (event) => {
                                 if(event !== null){
                                     if(event.rows.length > 0){
-                                        var temp = "{\"event_id\": " + event.rows[0].event_id + ", \"name\": \"" + event.rows[0].name + "\", \"description\": \""+ event.rows[0].description + "\", \"date\": \"" + event.rows[0].date +"\"}";
-                                        array.push(temp);
+                                        array.push(JSON.parse("{\"event_id\": " + event.rows[0].event_id + ", \"name\": \"" + event.rows[0].name + "\", \"description\": \""+ event.rows[0].description + "\", \"date\": \"" + event.rows[0].date +"\"}"));
                                         console.log(array);
                                     }
                                 }
