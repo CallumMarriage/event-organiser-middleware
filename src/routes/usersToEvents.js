023df59803927100dd0df757dd5ce07a4bc8e497
Callum Marriage <callum.marriage@capgemini.com>
2018-08-12 17:46:27 +0100

    added filters and functions

--- src/routes/usersToEvents.js
+++ src/routes/usersToEvents.js
@@ -1,4 +1,4 @@
-import { getUserByUsername} from '../models/users.js';
+import { getUserByUsername, getUserById} from '../models/users.js';
 import { getEventsByUser, getUsersToEvents, insertUserToEvent, getUniqueEvents, getNumberOfSubscribers, getSubscribersByEvent } from '../models/usersToEvents.js';
 import { getEventsByName, getEventsById} from '../models/events.js';
 const { sanitizeBody } = require('express-validator/filter');
