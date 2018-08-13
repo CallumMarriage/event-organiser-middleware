47954910892fb0fadc6ea471779f2f654373d737
Callum Marriage <callum.marriage@capgemini.com>
2018-08-13 12:01:00 +0100

    added filters and functions

--- src/routes/usersToEvents.js
+++ src/routes/usersToEvents.js
@@ -56,6 +56,7 @@ export function getEventsBySubscriberRoute(req, res){
             var user_id = user.rows[0].user_id;
             getEventsByUser(req.id, user_id, (events) => {
                 if(events !== null){
+                    console.log(">>> number of events: " + events.rows.length);
                     if(events.rows.length > 0){
                         var array = [];
                         var i;
