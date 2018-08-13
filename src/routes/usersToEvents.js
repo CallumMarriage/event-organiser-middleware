255a5e6b3902d3e4bd5ba3d78793be209d82c370
Callum Marriage <callum.marriage@capgemini.com>
2018-08-13 11:42:22 +0100

    added filters and functions

--- src/routes/usersToEvents.js
+++ src/routes/usersToEvents.js
@@ -116,11 +116,9 @@ export function postNewRelationshipRoute(req, res){
 
   getUserByUsername(req.id, username, (user) => {
     if(user !== null){
-        console.log(user);
-
         if(user.rows.length === 1){
             var user_id = user.rows[0].user_id;
-            console.log("user: " + user);   
+            console.log("user: " + user.rows[0]);   
             getEventsByName(req.id, eventName, (event) =>{
                 if(event.rows.length === 1){
                     var event_id = event.rows[0].event_id;
@@ -133,13 +131,19 @@ export function postNewRelationshipRoute(req, res){
                                     res.status(404).json({error: 'Invalid subscription attempt'});
                                 }
                             });
-                        } 
+                        } else {
+                            res.status(400).json({error: "You have already subscribed to this event"});
+                        }
                     });
+                } else {
+                    res.status(404).json({error: "Event does not exist"});
                 }
             });
         } else {
             res.status(404).json({error: "User does not exist"});
         }
+    } else {
+        res.status(500).json({error: "Internal Server Error"});
     }
   });
 }
